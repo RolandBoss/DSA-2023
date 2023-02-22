ec9ea4e716d70ea4902a8521a9952392753bd47b
RolandBoss <125950913+RolandBoss@users.noreply.github.com>
2023-02-22 10:39:48 +0200

    Lab2_Fel1

---
+++ Lab2/headers/functions_matrix.h
@@ -0,0 +1,8 @@
+//
+// Created by Roland on 2023. 02. 22..
+//
+
+#ifndef LAB2_FUNCTIONS_MATRIX_H
+#define LAB2_FUNCTIONS_MATRIX_H
+
+#endif //LAB2_FUNCTIONS_MATRIX_H
---
+++ Lab2/src/functions_matrix.c
@@ -0,0 +1,5 @@
+//
+// Created by Roland on 2023. 02. 22..
+//
+
+#include "../headers/functions_matrix.h"
