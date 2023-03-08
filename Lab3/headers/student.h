372ab51eab416ea3401a067aee67ef2b728bedf6
RolandBoss <125950913+RolandBoss@users.noreply.github.com>
2023-03-08 11:37:18 +0200

    Lab2_Fel1

--- Lab3/headers/student.h
+++ Lab3/headers/student.h
@@ -4,13 +4,10 @@
 
 #ifndef LAB3_STUDENT_H
 #define LAB3_STUDENT_H
-#include <stdio.h>
-typedef enum{
-    MALE,FEMALE
-}Gender;
+enum Gender {MALE,FEMALE, OTHER};
 
 typedef struct {
-    int year, month, day
+    int year, month, day;
 }Date_t;
 
 typedef struct {
@@ -18,12 +15,15 @@ typedef struct {
     char neptunCode[8];
     char birthPlace[25];
     Date_t dateOfBirth;
-    Gender gender;
+    enum Gender gender;
     float examResult;
 }Student_t;
 
 #endif //LAB3_STUDENT_H
 
-char* getGenderDescription(Gender gender);
+char* getGenderDescription(enum Gender type);
 void readStudentDetails(Student_t *pStudent);
 void printStudent(Student_t student);
+void allocateMemoryForStudents(Student_t **dpStudents, int numberOfStudents);
+void readAllStudentsDetails(Student_t **dpStudents, int *numberOfStudents, const char *input);
+void printAllStudents(Student_t *pStudents, int numberOfStudents, const char *destination);
--- Lab3/src/main.c
+++ Lab3/src/main.c
@@ -1,9 +1,22 @@
-
 #include "student.h"
+#include <stdio.h>
 
 int main() {
-    Student_t student1;
-    readStudentDetails(&student1);
-    printStudent(student1);
+ //   Student_t student1 = {"Adam Batyam", "AW6H4a", "Targu Mures",{2000,12,13}, MALE, 8.45f};
+   // printStudent(student1);
+    Student_t student2;
+    if(!freopen("students.txt", "r", stdin)){
+        printf("\nERROR");
+        return -1;
+    }
+    readStudentDetails(&student2);
+    freopen("CON", "r", stdin);
+    printStudent(student2);
+
+    Student_t *pStudents;
+    int numberOfStudents;
+    readAllStudentsDetails(&pStudents,&numberOfStudents,"students.txt");
+    printf("\n*****************************************\nAll students\n");
+    printAllStudents(pStudents, numberOfStudents,"CON");
     return 0;
 }
