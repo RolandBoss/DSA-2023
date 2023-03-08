f871dfa6b4f7253df93b1273f1e7ece9332f1a08
RolandBoss <125950913+RolandBoss@users.noreply.github.com>
2023-03-08 11:37:18 +0200

    Lab2_Fel1

--- Lab3/src/student.c
+++ Lab3/src/student.c
@@ -3,21 +3,30 @@
 //
 
 #include "student.h"
+#include <stdio.h>
+#include <malloc.h>
 
 void readStudentDetails(Student_t *pStudent){
-    scanf("%[^\n]\n", pStudent->name);
+    scanf("%[^\n]", pStudent->name);
     scanf("%s\n", pStudent->neptunCode);
     scanf("%[^\n]\n", pStudent->birthPlace);
-    scanf("%i", pStudent->dateOfBirth.year);
-    scanf("%i", pStudent->dateOfBirth.month);
-    scanf("%i", pStudent->dateOfBirth.day);
-    scanf("%i", pStudent->gender);
-    scanf("%f", pStudent->examResult);
+    scanf("%i%i%i", &pStudent->dateOfBirth.year,&pStudent->dateOfBirth.month, &pStudent->dateOfBirth.day);
+    scanf("%i\n", &pStudent->gender);
+    scanf("%f\n", &pStudent->examResult);
 }
-char* getGenderDescription(Gender gender){
-    switch (gender) {
+
+void allocateMemoryForStudents(Student_t **dpStudents, int numberOfStudents){
+    *dpStudents = (Student_t*)malloc(numberOfStudents * sizeof (Student_t));
+    if(!(*dpStudents)){
+        printf("\nError");
+        exit(-1);
+    }
+}
+char* getGenderDescription(enum Gender type){
+    switch (type) {
         case 0: return "MALE";
         case 1: return "FEMALE";
+        case 2: return "OTHER";
         default: return "ERROR GENDER NOT FOUND";
     }
 }
@@ -27,7 +36,28 @@ void printStudent(Student_t student){
     printf("Student neptun code: %s\n", student.neptunCode);
     printf("Student birth place: %s\n", student.birthPlace);
     printf("Student date of birt: %i.%i.%i\n", student.dateOfBirth.year,student.dateOfBirth.month,student.dateOfBirth.day);
-    printf("Gender: %s", getGenderDescription(student.gender));
-    printf("Exam result: %f\n", student.examResult);
+    printf("Gender: %s\n", getGenderDescription(student.gender));
+    printf("Exam result: %.2f\n", student.examResult);
+}
+
+void readAllStudentsDetails(Student_t **dpStudents, int *pnumberOfStudents, const char *input){
+    if(!freopen("input","r", stdin)){
+        printf("ERROREEEE");
+        exit(-2);
+    }
+    scanf("%i", pnumberOfStudents);
+    allocateMemoryForStudents(dpStudents, *pnumberOfStudents);
+    for (int i = 0; i < *pnumberOfStudents; ++i) {
+        readStudentDetails((*dpStudents)+i);
+    }
+    freopen("CON", "r", stdin);
+
+}
+void printAllStudents(Student_t *pStudents, int numberOfStudents, const char *destination){
+    freopen("destination", "w", stdout);
+    for (int i = 0; i < numberOfStudents; ++i) {
+        printStudent(pStudents[i]);
+    }
+    freopen("CON", "w", stdout);
 }
 
